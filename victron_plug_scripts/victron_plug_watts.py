51680fdd32ea15f067c346d448d48abc1d83a484
r09491 <r09491@t-online.de>
2023-12-14 19:42:50 +0100

    Changed load off prediction method

--- victron_plug_scripts/victron_plug_watts.py
+++ victron_plug_scripts/victron_plug_watts.py
@@ -7,12 +7,15 @@ import numpy as np
 
 from datetime import datetime, timedelta
 
-V_SAMPLES = 60 # 1 hour
+
+V_SAMPLES = 120 # 2 hour
 V_LOAD_OFF = 12.1 # V
 V_LOAD_DISCHARGE = 12.8 #V
 
+
 def strip_unit(value):
     return float(value[:-1])
+
 
 def estimate_load_off(f = sys.stdin):
     sep = '\t'
@@ -27,16 +30,17 @@ def estimate_load_off(f = sys.stdin):
     """ The battery voltage samples """
     vl = np.array(vldf.VL.apply(strip_unit))
 
+    samples = len(vl)
+
     """ The power samples """
     pl = np.array(vldf.PL.apply(strip_unit))
     p = np.array(vldf.P.apply(strip_unit))
     ppv = np.array(vldf.PPV.apply(strip_unit))
 
-    
-    """ Calculate the means  """
-    pl_mean = np.mean(pl)
-    p_mean = np.mean(p)
-    ppv_mean = np.mean(ppv)
+    """ Calculate the means over the last hour """
+    pl_mean = np.mean(pl[int(samples/2):])
+    p_mean = np.mean(p[int(samples/2):])
+    ppv_mean = np.mean(ppv[int(samples/2):])
 
     text = f'TIME {t[-1]}'
     text += f' SOLAR {ppv_mean:.1f} W'
@@ -45,36 +49,19 @@ def estimate_load_off(f = sys.stdin):
     text += f' SYS {-ppv_mean + p_mean + pl_mean:.1f} W'
     text += f' VL {vl[-1]:.2f} V'    
 
-
-    if vl[0] < V_LOAD_DISCHARGE:
+    if vl[0] > vl[-1] and vl[0] < V_LOAD_DISCHARGE and samples >= V_SAMPLES:
         
         """ Forecast LOAD OFF time for discharge """
 
-        samples = len(vl)
-        vl_minutes = np.arange(samples)
-        fit = np.polyfit(vl_minutes, vl, 1)
-        line = np.poly1d(fit)
+        mid = int(samples/2)
+        v0, v1 = vl[:mid].mean(), vl[mid:].mean()
+        load_off_hours = (V_LOAD_OFF - v1) / (v1 - v0)  
 
-        """ Find the minute when to stop loading in the next 24h """
-    
-        load_off_minute = None
-        for first_minute in range(samples, 23*samples, samples):
-            """ The minutes of the next period """
-            next_minutes = vl_minutes + first_minute
-            """ The estimates of the next period """
-            next_volts = line(next_minutes)
-            """ The values larger than the load off """
-            checked_minutes = next_minutes[next_volts > V_LOAD_OFF]
-            if len(checked_minutes) < samples:
-                load_off_minute = checked_minutes[-1]
-                break
+        text += f' LOAD OFF {load_off_hours:.1f} h'
 
-        if load_off_minute is not None:
-            text += f' LOAD OFF {load_off_minute/60:.1f} h'
-
-            load_off_time = datetime.strptime(t[-1], "%H:%M") + \
-                timedelta(minutes = int(load_off_minute))
-            text += f' @ {load_off_time.strftime("%H:%M")}'
+        load_off_time = datetime.strptime(t[-1], "%H:%M") + \
+                timedelta(minutes = int(load_off_hours * 60))
+        text += f' @ {load_off_time.strftime("%H:%M")}'
 
     print(text)
 
